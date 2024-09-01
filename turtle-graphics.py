a03432d1fac43c4360fa9e04e5537b43b5abda08
Raghuraj Singh <hannuseven@gmail.com>
2024-09-01 19:52:24 +0530

    turtle

---
+++ turtle-graphics.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+print(t)
+t.shape("turtle")
+t.color("pink")
+t.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
